d30e1664b70c6f0fa4432e31409a7e699fe82422
AlanharS3 <alanharsufi333@gmail.com>
2022-06-07 02:50:09 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,6 @@ Route::post('/login',[ LoginController::class, 'authenticate']);
 Route::get('/register',[ RegisterController::class, 'register']);
 Route::post('/register',[ RegisterController::class, 'store']);
 
-Route::get('/dashboard',[ DashboardController::class, 'index']);
-
 //Route::get('/register', function () {
     //return view('register');
 //});
