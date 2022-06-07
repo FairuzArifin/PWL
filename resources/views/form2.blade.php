bf1539d961a471551653aa0695e1937dfeb917ea
PutriAnisa123 <pa374167@gmail.com>
2022-06-07 13:13:00 +0700

    terbaru

--- resources/views/form2.blade.php
+++ resources/views/form2.blade.php
@@ -9,33 +9,58 @@
 		<div class="col-md-9">
 			<div class="contact-form">
 				<div class="form-group">
-				  <label class="control-label col-sm-2" for="fname">First Name:</label>
+                <form class = "form-control">
+                    <span class="contact100-form-title">Form Pertanyaan</span>
+				  <label class="control-label col-sm-2" >Nama</label>
 				  <div class="col-sm-10">          
-					<input type="text" class="form-control" id="fname" placeholder="Enter First Name" name="fname">
+					<input type="text" class="form-control" id="name" placeholder="Masukkan Nama Anda" name="name" required>
 				  </div>
 				</div>
+
 				<div class="form-group">
-				  <label class="control-label col-sm-2" for="lname">Last Name:</label>
+				  <label class="control-label col-sm-2" >NIK</label>
 				  <div class="col-sm-10">          
-					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name" name="lname">
+					<input type="text" class="form-control" id="nik" placeholder="Masukkan NIK Anda" name="nik" required>
 				  </div>
 				</div>
+
 				<div class="form-group">
-				  <label class="control-label col-sm-2" for="email">Email:</label>
+				  <label class="control-label col-sm-2" for="email">Jenis Kelamin</label>
 				  <div class="col-sm-10">
-					<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                  <select class="form-select " name="jenis_kelamin">
+                        <option>Please chooses</option>
+                        <option>Laki - Laki</option>
+                        <option>Perempuan</option>
+                    </select>
 				  </div>
 				</div>
+
+                <div class="form-group">
+				  <label class="control-label col-sm-2" >Alamat</label>
+				  <div class="col-sm-10">          
+					<input type="text" class="form-control" id="alamat" placeholder="Masukkan Alamat Anda" name="alamat" required>
+				  </div>
+				</div>
+
+                <div class="form-group">
+				  <label class="control-label col-sm-2" >Nomor Hp</label>
+				  <div class="col-sm-10">          
+					<input type="text" class="form-control" id="no_hp" placeholder="Masukkan Alamat Anda" name="no_hp" required>
+				  </div>
+				</div>
+
 				<div class="form-group">
-				  <label class="control-label col-sm-2" for="comment">Comment:</label>
+				  <label class="control-label col-sm-2" for="comment">Pertanyaan</label>
 				  <div class="col-sm-10">
-					<textarea class="form-control" rows="5" id="comment"></textarea>
+					<textarea class="form-control" rows="5" id="pertanyaan" name="pertanyaan"></textarea>
 				  </div>
 				</div>
+
 				<div class="form-group">        
 				  <div class="col-sm-offset-2 col-sm-10">
-					<button type="submit" class="btn btn-default">Submit</button>
+					<button type="submit" class="btn btn-">Submit</button>
 				  </div>
+                    </form>
 				</div>
 			</div>
 		</div>
