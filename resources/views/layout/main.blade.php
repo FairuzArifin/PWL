c4776672bf3039afd3ce23d794531d49cd508e3d
AlanharS3 <alanharsufi333@gmail.com>
2022-06-07 15:22:53 +0700

    tes

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Desa</title>
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="bar/css/style.css">    
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
